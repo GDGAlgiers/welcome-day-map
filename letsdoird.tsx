b77f32c784f53c66e867b94acc7c82369a56dd26
akram09 <a.boutouchent@esi-sba.dz>
2021-12-24 22:22:24 +0100

    Fixing scrollbars on countdown

--- letsdoird.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="letsdoird" tilewidth="32" tileheight="32" tilecount="12" columns="4">
- <image source="src/assets/letsdroid.png" width="128" height="117"/>
-</tileset>
